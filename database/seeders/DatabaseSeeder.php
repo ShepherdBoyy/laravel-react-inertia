2418e5010d8b9d4f574453514ed37fe5c54e05de
ShepherdBoyy <shepherd052603@gmail.com>
2025-09-02 04:54:31 +0800

    Add factories for Task Factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,10 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Cowtherager',
+            'email' => 'cow@example.com',
+            'password' => bcrypt('123.321A'),
+            'email_verified_at' => time()
         ]);
     }
 }
