c15b8fcac399e46d89b09ebeb883b5c99d1a6256
ShepherdBoyy <shepherd052603@gmail.com>
2025-09-05 20:53:48 +0800

    Render data in project page

---
+++ app/Http/Resources/TaskResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TaskResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            
+        ];
+    }
+}
