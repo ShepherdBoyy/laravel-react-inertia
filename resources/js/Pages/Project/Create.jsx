dd38f497060bed1744b207ded318068539d3eb54
ShepherdBoyy <shepherd052603@gmail.com>
2025-09-07 17:53:48 +0800

    Initialize the template of the add new form

---
+++ resources/js/Pages/Project/Create.jsx
@@ -0,0 +1,29 @@
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Head } from "@inertiajs/react";
+
+export default function Create({ auth }) {
+    return (
+        <AuthenticatedLayout
+            user={auth.user}
+            header={
+                <div className="flex justify-between items-center">
+                    <h2 className="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
+                        Create New Project
+                    </h2>
+                </div>
+            }
+        >
+            <Head title="Create New Project" />
+
+            <div className="py-12">
+                <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                    <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
+                        <div className="p-6 text-gray-900 dark:text-gray-100">
+                            Form
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </AuthenticatedLayout>
+    )
+}
