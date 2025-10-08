a350e20db4dd728cc6a3d8ffc2273c0872567bb3
ShepherdBoyy <shepherd052603@gmail.com>
2025-10-08 22:14:28 +0800

    Add users crud

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,15 +5,34 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use App\Http\Requests\StoreUserRequest;
 use App\Http\Requests\UpdateUserRequest;
+use App\Http\Resources\UserCrudResource;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        $query = User::query();
+
+        $sortField = request("sort_field", "created_at");
+        $sortDirection = request("sort_direction", "desc");
+
+        if (request("name")) {
+            $query->where("name", "like", "%". request("name") ."%");
+        }
+        if (request("email")) {
+            $query->where("email", "like", "%". request("email") ."%");
+        }
+
+        $users = $query
+            ->orderBy($sortField, $sortDirection)
+            ->paginate(10)
+            ->onEachSide(1);
+
+        return inertia('User/Index', [
+            "users" => UserCrudResource::collection($users),
+            "queryParams" => request()->query() ?: null,
+            "success" => session("success")
+        ]);
     }
 
     /**
@@ -21,7 +40,7 @@ class UserController extends Controller
      */
     public function create()
     {
-        //
+        return inertia("User/Create");
     }
 
     /**
@@ -29,7 +48,12 @@ class UserController extends Controller
      */
     public function store(StoreUserRequest $request)
     {
-        //
+        $data = $request->validated();
+        $data["password"] = bcrypt($data["password"]);
+        User::create($data);
+
+        return to_route('user.index')
+            ->with('success', 'User was created');
     }
 
     /**
---
+++ resources/js/Pages/User/Edit.jsx
@@ -0,0 +1,137 @@
+import InputError from "@/Components/InputError";
+import InputLabel from "@/Components/InputLabel";
+import SelectInput from "@/Components/SelectInput";
+import TextAreaInput from "@/Components/TextAreaInput";
+import TextInput from "@/Components/TextInput";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Head, Link, useForm } from "@inertiajs/react";
+
+export default function Edit({ auth, user }) {
+    const {data, setData, post, errors, reset} = useForm({
+        image: "",
+        name: user.name || "",
+        status: user.status || "",
+        description: user.description || "",
+        due_date: user.due_date || "",
+        _method: "PUT"
+    })
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        post(route("user.update", user.id));
+    }
+
+    return (
+        <AuthenticatedLayout
+            user={auth.user}
+            header={
+                <div className="flex justify-between items-center">
+                    <h2 className="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
+                        Edit user "{user.name}"
+                    </h2>
+                </div>
+            }
+        >
+            <Head title="Edit User" />
+
+            <div className="py-12">
+                <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                    <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
+                        <form
+                            className="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg"
+                            onSubmit={handleSubmit}
+                        >
+                            {user.image_path && (
+                                <div>
+                                    <img src={user.image_path} className="w-64" />
+                                </div>
+                            )}
+                            <div>
+                                <InputLabel htmlFor="user_image_path" value="User Image" />
+                                <TextInput
+                                    id="user_image_path"
+                                    type="file"
+                                    name="image"
+                                    className="mt-1 block w-full"
+                                    onChange={e => setData('image', e.target.files[0])}
+                                />
+                                <InputError message={errors.image} className="mt-2" />
+                            </div>
+                            
+                            <div className="mt-4">
+                                <InputLabel htmlFor="user_name" value="User Name" />
+                                <TextInput
+                                    id="user_name"
+                                    type="text"
+                                    name="name"
+                                    value={data.name}
+                                    className="mt-1 block w-full"
+                                    isFocused={true}
+                                    onChange={e => setData('name', e.target.value)}
+                                />
+                                <InputError message={errors.name} className="mt-2" />
+                            </div>
+
+                            <div className="mt-4">
+                                <InputLabel htmlFor="user_description" value="User Description" />
+                                <TextAreaInput
+                                    id="user_description"
+                                    type="text"
+                                    name="description"
+                                    value={data.description}
+                                    className="mt-1 block w-full"
+                                    isFocused={true}
+                                    onChange={e => setData('description', e.target.value)}
+                                />
+                                <InputError message={errors.description} className="mt-2" />
+                            </div>
+
+                            <div className="mt-4">
+                                <InputLabel htmlFor="user_due_date" value="User Deadline" />
+                                <TextInput
+                                    id="user_due_date"
+                                    type="date"
+                                    name="date"
+                                    value={data.due_date}
+                                    className="mt-1 block w-full"
+                                    isFocused={true}
+                                    onChange={e => setData('due_date', e.target.value)}
+                                />
+                                <InputError message={errors.due_date} className="mt-2" />
+                            </div>
+
+                            <div className="mt-4">
+                                <InputLabel htmlFor="user_status" value="User Status" />
+                                <SelectInput
+                                    id="user_status"
+                                    name="status"
+                                    className="mt-1 block w-full"
+                                    onChange={e => setData('status', e.target.value)}
+                                >
+                                    <option value="">Select Status</option>
+                                    <option value="pending">Pending</option>
+                                    <option value="in_progress">In Progress</option>
+                                    <option value="completed">Completed</option>
+                                </SelectInput>
+                                <InputError message={errors.user_status} className="mt-2" />
+                            </div>
+
+                            <div className="mt-4 text-right">
+                                <Link
+                                    href={route("user.index")} 
+                                    className="bg-gray-100 py-1 px-3 text-gray-800 rounded shadow transition-all hover:bg-gray-200 mr-2"
+                                >
+                                    Cancel
+                                </Link>
+                                <button className="bg-emerald-500 py-1 px-3 text-white rounded shadow transition-all hover:bg-emerald-600">
+                                    Submit
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </AuthenticatedLayout>
+    )
+}
